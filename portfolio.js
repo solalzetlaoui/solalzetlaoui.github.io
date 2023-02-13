452a9c7bbcf875fe7a31f10294ee53404f2fef22
Solal <30844381+Praydd@users.noreply.github.com>
2023-02-13 20:57:31 +0100

    Added new files

---
+++ portfolio.js
@@ -0,0 +1,16 @@
+window.onscroll = function() {
+  let headerMenu = document.querySelector(".header-menu");
+  let headerMenuItems = document.querySelectorAll(".header-menu-item");
+
+  if (window.pageYOffset > 100) {
+    headerMenu.style.backgroundColor = "rgba(255, 255, 255, 1)";
+    headerMenuItems.forEach(function(item) {
+      item.style.color = "black";
+    });
+  } else {
+    headerMenu.style.backgroundColor = "rgba(0, 0, 0, 0)";
+    headerMenuItems.forEach(function(item) {
+      item.style.color = "white";
+    });
+  }
+};
