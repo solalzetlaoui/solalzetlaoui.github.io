58af52f4f7719416c1537c3b1750d430d86a645c
Solal <30844381+solalzetlaoui@users.noreply.github.com>
2025-07-30 23:10:23 +0200

    Fix Jekyll build errors and setup GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,4 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
-  site: 'https://solalzetlaoui.github.io',
-  base: '/',
-});
+export default defineConfig({});
